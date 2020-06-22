156947483a8bc848c42c9ebcad0be1e7c3ef9d45
gaurav04 <harsola.gaurav88@gmail.com>
2020-06-22 20:24:45 +0530

    adding code to create kafka topics

---
+++ kafka/kafka.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"log"
+
+	"github.com/Shopify/sarama"
+)
+
+func main() {
+	brokerAddrs := []string{"192.168.43.30:9092"}
+	config := sarama.NewConfig()
+	config.Version = sarama.V0_10_2_1
+	admin, err := sarama.NewClusterAdmin(brokerAddrs, config)
+	if err != nil {
+		log.Fatal("Error while creating cluster admin: ", err.Error())
+	}
+	defer admin.Close()
+
+	err = admin.CreateTopic("gaurav", &sarama.TopicDetail{
+		NumPartitions:     1,
+		ReplicationFactor: 1,
+	}, false)
+	if err != nil {
+		log.Fatal("Error while creating topic: ", err.Error())
+	}
+}
