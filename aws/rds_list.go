edf4f6f9db4fcef7a91cce5c4b604edccc512bc5
gaurav04 <harsola.gaurav88@gmail.com>
2020-06-22 16:04:17 +0530

    adding rds list code

---
+++ aws/rds_list.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/rds"
+)
+
+func main() {
+
+	var region string
+	var accessKey string
+	var secretKey string
+	flag.StringVar(&accessKey, "accesskey", "", "access key")
+	flag.StringVar(&secretKey, "secretkey", "", "secret-key")
+	flag.StringVar(&region, "region", "us-west-2", "aws region")
+	//flag.parse()
+	flag.Parse()
+
+	sess, err := session.NewSession(&aws.Config{Region: aws.String(region), Credentials: credentials.NewStaticCredentials(accessKey, secretKey, "")})
+
+	// Create RDS service client
+	svc := rds.New(sess)
+
+	result, err := svc.DescribeDBInstances(nil)
+	if err != nil {
+		fmt.Println(err.Error)
+	}
+
+	fmt.Println("Instances:")
+
+	for _, d := range result.DBInstances {
+		fmt.Printf("* %s created on %s\n",
+			aws.StringValue(d.DBInstanceIdentifier), aws.TimeValue(d.InstanceCreateTime))
+	}
+}
