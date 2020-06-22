fb3181085d6e4bde3d45d3fb1ded7cf534c80b47
gaurav04 <harsola.gaurav88@gmail.com>
2020-06-22 12:40:07 +0530

    list all ec2 instance

---
+++ aws/ec2.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/ec2"
+)
+
+func main() {
+	// Load session from shared config
+	var region string
+	var accessKey string
+	var secretKey string
+	flag.StringVar(&accessKey, "accesskey", "", "access key")
+	flag.StringVar(&secretKey, "secretkey", "", "secret-key")
+	flag.StringVar(&region, "region", "us-west-2", "aws region")
+	//flag.parse()
+	flag.Parse()
+
+	sess, err := session.NewSession(&aws.Config{Region: aws.String(region), Credentials: credentials.NewStaticCredentials(accessKey, secretKey, "")})
+	// Create new EC2 client
+	ec2Svc := ec2.New(sess)
+
+	// Call to get detailed information on each instance
+	result, err := ec2Svc.DescribeInstances(nil)
+	if err != nil {
+		fmt.Println("Error", err)
+	} else {
+		fmt.Println("Success", result)
+	}
+
+	for _, r := range result.Reservations {
+		fmt.Println("Reservation ID: " + *r.ReservationId)
+		fmt.Println("Instance IDs:")
+		for _, i := range r.Instances {
+			fmt.Println("   " + *i.InstanceId)
+		}
+
+		fmt.Println("")
+	}
+
+}
