d37cab69fb4c2ce3d90d698a62c0910ceb3629e3
gaurav04 <harsola.gaurav88@gmail.com>
2020-06-25 12:22:38 +0530

    adding zookeeper client

---
+++ zookeeper/zookeeper-client.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+	"time"
+
+	"github.com/samuel/go-zookeeper/zk"
+)
+
+func must(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func connect() *zk.Conn {
+	zksStr := "localhost:2181"
+	zks := strings.Split(zksStr, ",")
+	conn, _, err := zk.Connect(zks, time.Second)
+	must(err)
+	return conn
+}
+
+func main() {
+	conn := connect()
+	data, _, _ := conn.Children("/brokers/topics/senz/partitions")
+	fmt.Println("Total Partition Count", len(data))
+	for _, partitions := range data {
+		fmt.Println(partitions)
+	}
+}
